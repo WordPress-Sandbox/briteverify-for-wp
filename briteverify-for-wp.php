022f47f353ba64a7c5db67f51b72a956cab1e27e
David <david@shellcreeper.com>
2017-01-14 23:43:11 +0700

    add main plugin file.

---
+++ briteverify-for-wp.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: BriteVerify for WP
+ * Plugin URI: http://astoundify.com/downloads/briteverify-for-wp/
+ * Description: BriteVerify email verification for WordPress.
+ * Version: 1.0.0
+ * Author: Astoundify
+ * Author URI: http://astoundify.com/
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: briteverify-for-wp
+ * Domain Path: /languages/
+**/
+if ( ! defined( 'WPINC' ) ) { die; }
